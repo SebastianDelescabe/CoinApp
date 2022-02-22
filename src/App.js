748f986b7e1188ced3afc37ab3a29f73c130fa26
Sebastian <sebastiandelescabe@gmail.com>
2022-02-21 21:23:29 -0300

    end project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import TableCoins from './components/TableCoins';
+
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  const getData = async () => {
+    const apiData = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
+    setCoins(apiData.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+        <h1 className=' text-light mt-4 text-center'>Coin Market</h1>
+        <input
+          type='text'
+          placeholder='Search Coin'
+          className='form-control bg-dark text-light border-0 mt-4 text-center'
+          onChange={e => setSearch(e.target.value)} />
+        <TableCoins coins={coins} search={search} />
+      </div>
     </div>
   );
 }
